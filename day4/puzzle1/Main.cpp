6fc46525754bed6bd5ba0356db04a8ff8cc829fc
Brayden Rayner <braydenrayner@outlook.com>
2022-12-04 18:59:49 -0400

    Added files for day 4 - puzzle 1

---
+++ day4/puzzle1/Main.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <gtest/gtest.h>
+using namespace std;
+
+struct SectionRange
+{
+	const int lower;
+	const int upper;
+	SectionRange(int lower, int upper);
+};
+
+SectionRange::SectionRange(int lower, int upper) : upper {upper}, lower {lower} {}
+
+using SectionRangePair = pair<SectionRange, SectionRange>;
+
+SectionRange parseSectionRange(const string& raw)
+{
+	auto hyphen = raw.find('-');
+	auto lower = raw.substr(0, hyphen);
+	auto upper = raw.substr(hyphen + 1);
+	return SectionRange(stoi(lower), stoi(upper));
+}
+
+vector<SectionRangePair> loadInputs(const string& path)
+{
+	ifstream inputFile(path);
+	if (!inputFile.is_open())
+		throw runtime_error("Failed to open input file.");
+
+	vector<SectionRangePair> inputs;
+
+	string line;
+	while (getline(inputFile, line))
+	{
+		auto comma = line.find(',');
+		auto range1 = line.substr(0, comma);
+		auto range2 = line.substr(comma + 1);
+		inputs.push_back({parseSectionRange(range1), parseSectionRange(range2)});
+	}
+
+	inputFile.close();
+	return inputs;
+}
+
+bool rangesOverlap(const SectionRangePair& ranges)
+{
+	auto& r1 = ranges.first;
+	auto& r2 = ranges.second;
+
+	if (r1.lower >= r2.lower && r1.upper <= r2.upper)
+		return true;
+
+	if (r2.lower >= r1.lower && r2.upper <= r1.upper)
+		return true;	
+
+	return false;
+}
+
+TEST(SectionTests, HandlesSample1)
+{
+	SectionRange r1 {2, 4};
+	SectionRange r2 {6, 8};
+	EXPECT_EQ(rangesOverlap({r1, r2}), false);
+}
+
+TEST(SectionTests, HandlesSample3)
+{
+	SectionRange r1 {3, 7};
+	SectionRange r2 {2, 8};
+	EXPECT_EQ(rangesOverlap({r1, r2}), true);
+}
+
+TEST(SectionTests, HandlesSample2)
+{
+	SectionRange r1 {6, 6};
+	SectionRange r2 {4, 6};
+	EXPECT_EQ(rangesOverlap({r1, r2}), true);
+}
+
+int main(int argc, char *argv[])
+{
+	testing::InitGoogleTest(&argc, argv);
+
+	try
+	{
+		int overlaps = 0;
+		for (auto& ranges : loadInputs("input/inputs.txt"))
+		{
+			if (rangesOverlap(ranges))
+				overlaps++;
+		}
+
+		cout << "Total Overlaps: " << overlaps << '\n';
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	return RUN_ALL_TESTS();
+}
